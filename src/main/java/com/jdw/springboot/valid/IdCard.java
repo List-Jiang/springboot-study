61771d915aaa0e556af16d49c2a3b57531fb89b0
2022099 <dewen.jiang@chinacreator.com>
2022-07-19 18:23:08 +0800

    添加身份证校验

---
+++ src/main/java/com/jdw/springboot/valid/IdCard.java
@@ -0,0 +1,31 @@
+package com.jdw.springboot.valid;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = IdCardValidator.class)
+public @interface IdCard {
+    String value();
+
+    String message() default "非法的身份证号码";
+
+    /**
+     * 将validator进行分类，不同的类group中会执行不同的validator操作
+     *
+     * @return validator的分类类型
+     */
+    Class<?>[] groups() default {};
+
+    /**
+     * 主要是针对bean，很少使用
+     *
+     * @return 负载
+     */
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/jdw/springboot/valid/IdCardValidator.java
@@ -0,0 +1,44 @@
+package com.jdw.springboot.valid;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
+
+public class IdCardValidator implements ConstraintValidator<IdCard, CharSequence> {
+    private static final int[] POWERS = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+    // 余数对应的最后一位校验码
+    private static final char[] CODE = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
+
+    private static final Pattern PROVINCIAL_PATTERN = Pattern.compile("1[1-5]|2[123]|3[1-7]|4[1-6]|5[0-4]|6[1-5]|71|81|82");
+    private static final DateTimeFormatter yyyyMMdd = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+
+    @Override
+    public void initialize(IdCard constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        boolean state = false;
+        try {
+            if (PROVINCIAL_PATTERN.matcher(value.subSequence(0, 2)).matches() && (value.length() == 15 || value.length() == 18)) {
+                LocalDate.parse(value.subSequence(6, 14), yyyyMMdd);
+                if (value.length() == 18) {
+                    int sum = 0;
+                    for (int i = 0; i < 17; i++) {
+                        sum += Integer.parseInt(((String) value).substring(i, i + 1)) * POWERS[i];
+                    }
+                    state = CODE[sum % 11] == value.charAt(17);
+                } else {
+                    state = true;
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return state;
+    }
+}
