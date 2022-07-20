d7226735885242c75131721f438fa0e438d74a5b
2022099 <dewen.jiang@chinacreator.com>
2022-07-20 10:38:58 +0800

    jackson 时间序列化配置更新

--- src/main/java/com/jdw/springboot/config/LocalDateTimeSerializerConfig.java
+++ src/main/java/com/jdw/springboot/config/LocalDateTimeSerializerConfig.java
@@ -1,5 +1,6 @@
 package com.jdw.springboot.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -7,11 +8,9 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,6 +20,7 @@ import java.time.format.DateTimeFormatter;
 /**
  * LocalDateTime序列化配置
  * 用于解决json转换时的格式问题
+ *
  * @author ListJiang
  * @since 2022/01/03
  */
@@ -29,38 +29,6 @@ public class LocalDateTimeSerializerConfig {
     private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
     private static final String DATE_PATTERN = "yyyy-MM-dd";
     private static final String TIME_PATTERN = "HH:mm:ss";
-
-    /**
-     * String 转 LocalDate 转换器
-     */
-    @Bean
-    public Converter<String, LocalDate> localDateConverter() {
-        // 此处 new Converter<String, LocalDate>() 不可简写
-        return new Converter<String, LocalDate>() {
-            @Override
-            public LocalDate convert(@NotNull String source) {
-                if (source.trim().length() == 0)
-                    return null;
-                return LocalDate.parse(source, DateTimeFormatter.ofPattern(DATE_PATTERN));
-            }
-        };
-    }
-
-    /**
-     * String 转 LocalDateTime 转换器
-     */
-    @Bean
-    public Converter<String, LocalDateTime> localDateTimeConverter() {
-        // 此处 new Converter<String, LocalDate>() 不可简写
-        return new Converter<String, LocalDateTime>() {
-            @Override
-            public LocalDateTime convert(@NotNull String source) {
-                if (source.trim().length() == 0)
-                    return null;
-                return LocalDateTime.parse(source, DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
-            }
-        };
-    }
 
     /**
      * 统一配置 LocalDate、LocalDateTime、LocalTime 与 String 之间的互相转换
@@ -76,16 +44,16 @@ public class LocalDateTimeSerializerConfig {
      */
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-        JavaTimeModule module = new JavaTimeModule();
-        module.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_PATTERN)));
-        module.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(TIME_PATTERN)));
-        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)));
-        return builder -> {
-            builder.simpleDateFormat(DATE_TIME_PATTERN);
-            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_PATTERN)));
-            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)));
-            builder.serializers(new LocalTimeSerializer(DateTimeFormatter.ofPattern(TIME_PATTERN)));
-            builder.modules(module);
-        };
+        var module = new JavaTimeModule()
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_PATTERN)))
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(TIME_PATTERN)))
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)));
+        return builder -> builder
+                .modules(module)
+                .simpleDateFormat(DATE_TIME_PATTERN)
+                .serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_PATTERN)))
+                .serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)))
+                .serializers(new LocalTimeSerializer(DateTimeFormatter.ofPattern(TIME_PATTERN)))
+                .serializationInclusion(JsonInclude.Include.NON_NULL);
     }
 }
